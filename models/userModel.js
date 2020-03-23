120efbc8737f544813730ca6a8529ea11235ab4f
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-23 20:01:34 +0100

    Add userModel properties and note to miranda in signup

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
+    firstName: String,
+    lastName: String,
+  },
+  { email: String },
+  {
     username: String,
     password: String,
   },
